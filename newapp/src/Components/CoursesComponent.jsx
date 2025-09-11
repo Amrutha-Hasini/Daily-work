0262d7a95cefba78723c4d7187d173b1c1d50fb8
unknown <amruthatelaprolu9@gmail.com>
2025-09-11 10:59:49 +0530

    exp 4,5

---
+++ newapp/src/Components/CoursesComponent.jsx
@@ -0,0 +1,14 @@
+CoursesComponent
+import React from "react";
+import StudentComponent from './Components/StudentComponent'
+function StudentComponent()
+{
+    return (
+        <>
+        <h1>Welcome to React</h1>
+        <StudentComponent/>
+        </>
+    )
+}
+
+export default CoursesComponent
---
+++ newapp/src/Components/StudentComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StudentComponent from './Components'
+function StudentComponent()
+{
+    return (
+        <>
+        <h1>Welcome to React</h1>
+        <CoursesComponent/>
+        </>
+    )
+}
+
+export default StudentComponent
